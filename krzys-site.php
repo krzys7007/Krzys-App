38e1ab88a0b5abb0444bee176ff43840de79835f
krzys7007 <krzys7007@gmail.com>
2024-03-29 01:42:33 +0100

    first iteration - going to sleep

--- krzys-site.php
+++ krzys-site.php
@@ -2,11 +2,45 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device=width, initial-scale=1.0">
+    <link rel="stylesheet" href="krzys_site.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title> Krzys-App </title>
 </head>
 
 <body>
-    A
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>A
+    <div class="banner">
+            <div class="guest_chat">
+            <button class="btn btn-outline-secondary btn-lg">≡</button>
+            </div>
+            <div class="logo">
+                Krzys-App Website
+                <img src="Assets/logo_png.png" class = "logo_png">
+            </div>
+    </div>
+    <div class="content">
+        <div class="my_photo">
+            <img src="Assets/logo_png.png" class = "my_photo_png">
+            <div class="my_photo_text">
+            Krzystof Nowakowski
+            </div>    
+        </div>
+        <div class="about_me">
+        About me...    
+            <div class="list">
+                <ul>
+                    <li>Interests</li>
+                    <li>Expirience</li>
+                    <li>CV <button class="btn btn-outline-secondary btn-lg"><img src="download_symbol.png" class = "download_symbol"></button> </li>
+                    <li>Skills</li>
+                </ul>
+            </div>
+        </div>
+        <div class="Skills">
+            Lorem Ipsum
+        </div>
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
